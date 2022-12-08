9aec0f08fdc69e75735b1f1713132e4ff22112a6
Sergey Shumakov <yohan2010@icloud.com>
2022-12-08 23:09:10 +0500

    v 0.3 fix bugs

--- main.py
+++ main.py
@@ -15,12 +15,12 @@ def input_stat():
         stick_count = input("Введите кол-во палочек (не меньше 5 штук): ")
     while True:
         if stick_count.isdigit():
-            print(f"Итак {name_first_player} Вы будете ходить первым, а {name_second_player} вторым. "
-                  f"Играть будем с {stick_count} палочками")
+            print(f"Итак {name_first_player.capitalize()} Вы будете ходить первым, а {name_second_player.capitalize()}"
+                  f" вторым. Играть будем с {stick_count} палочками")
             break
         else:
             stick_count = input("Введите кол-во палочек: ")
-    step_game(name_first_player, name_second_player, stick_count)
+    step_game(name_first_player.capitalize(), name_second_player.capitalize(), stick_count.capitalize())
 
 def restart_game():
     while True:
@@ -28,6 +28,7 @@ def restart_game():
         if restart.lower() == "y":
             input_stat()
         elif restart.lower() == "n":
+            print("До новых встреч!")
             quit()
 
 
@@ -41,16 +42,26 @@ def step_game(name_first_player, name_second_player, stick_count):
         if 0 < take_count <= 3:
             stick_count = int(stick_count) - int(take_count)
             if count_round % 2 != 0:
-                print(name_first_player, "взял", take_count, "шт. и в банке осталось", int(stick_count))
-                count_round += 1
+                if stick_count < 0:
+                    stick_count = 0
+                    print(f"{name_first_player} взял {take_count} шт. и в банке осталось {int(stick_count)}")
+                    count_round += 1
+                else:
+                    print(f"{name_first_player} взял {take_count} шт. и в банке осталось {int(stick_count)}")
+                    count_round += 1
             else:
-                print(name_second_player, "взял", take_count, "шт. и в банке осталось", int(stick_count))
-                count_round += 1
+                if stick_count < 0:
+                    stick_count = 0
+                    print(f"{name_second_player} взял {take_count} шт. и в банке осталось {int(stick_count)}")
+                    count_round += 1
+                else:
+                    print(f"{name_second_player} взял {take_count} шт. и в банке осталось {int(stick_count)}")
+                    count_round += 1
         else:
             take_count = int(input("Берите палочки(не больше 3 штук): "))
         if int(stick_count) <= 0:
             if count_round % 2 != 0:
-                print("Поздравляю с победой", name_first_player, ", игра завершилась через", count_round, "раундов!")
+                print(f"Поздравляю с победой {name_first_player} игра завершилась через {count_round} раундов!")
                 restart_game()
             else:
                 print(f'Поздравляю с победой {name_second_player}, игра завершилась через {count_round} раундов!')
