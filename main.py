bfbd58f197ed56ef20de7de46c094e4bb3727e7b
Sergey Shumakov <yohan2010@icloud.com>
2022-12-08 22:55:26 +0500

    v 0.2 fix bugs

--- main.py
+++ main.py
@@ -1,24 +1,43 @@
 #  designed by S1THOF
 
 def input_stat():
-    name_first_player, name_second_player = input("Введите имя первого игрока: "), input("Введите имя второго игрока: ")
+
+    name_first_player = input("Введите имя первого игрока: ")
+    while name_first_player == "":
+        name_first_player = input("Введите имя первого игрока: ")
+    name_second_player = input("Введите имя второго игрока: ")
+    while name_second_player == "":
+        name_second_player = input("Введите имя второго игрока: ")
     stick_count = input("Введите кол-во палочек:")
+    while not stick_count.isdigit():
+        stick_count = input("Введите кол-во палочек:")
     while int(stick_count) < 5:
         stick_count = input("Введите кол-во палочек (не меньше 5 штук): ")
     while True:
         if stick_count.isdigit():
-            print("Итак", name_first_player, "Вы будете ходить первым, а", name_second_player, "вторым.",
-                  "Играть будем с", stick_count, "палочками")
+            print(f"Итак {name_first_player} Вы будете ходить первым, а {name_second_player} вторым. "
+                  f"Играть будем с {stick_count} палочками")
             break
         else:
             stick_count = input("Введите кол-во палочек: ")
     step_game(name_first_player, name_second_player, stick_count)
 
+def restart_game():
+    while True:
+        restart = input("Начнем еще раз? (y/n): ")
+        if restart.lower() == "y":
+            input_stat()
+        elif restart.lower() == "n":
+            quit()
+
 
 def step_game(name_first_player, name_second_player, stick_count):
     count_round = 1
     while int(stick_count) != 0:
-        take_count = int(input("Берите палочки(не больше 3 штук): "))
+        if count_round % 2 != 0:
+            take_count = int(input(f"{name_first_player}, берите палочки(не больше 3 штук): "))
+        else:
+            take_count = int(input(f"{name_second_player}, берите палочки(не больше 3 штук): "))
         if 0 < take_count <= 3:
             stick_count = int(stick_count) - int(take_count)
             if count_round % 2 != 0:
@@ -32,12 +51,11 @@ def step_game(name_first_player, name_second_player, stick_count):
         if int(stick_count) <= 0:
             if count_round % 2 != 0:
                 print("Поздравляю с победой", name_first_player, ", игра завершилась через", count_round, "раундов!")
-                while True:
-                    restart = input("Начнем еще раз? (y/n): ")
-                    if restart.lower() == "y":
-                        input_stat()
-                    elif restart.lower() == "n":
-                        break
+                restart_game()
+            else:
+                print(f'Поздравляю с победой {name_second_player}, игра завершилась через {count_round} раундов!')
+                restart_game()
+
 
 print("Добро пожаловать в игру 'Палочки'!")
 input_stat()
